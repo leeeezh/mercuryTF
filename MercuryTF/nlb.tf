c1f4842ac1afe67d5277f93616a1c718c70b225a
Lee <37789078+leeeezh@users.noreply.github.com>
2022-05-05 11:03:51 +0800

    Add files via upload

--- MercuryTF/nlb.tf
+++ MercuryTF/nlb.tf
@@ -1,38 +1,45 @@
-#create nlb
+#create nlb ssl
 resource "aws_lb" "nlb" {
   name               = "mercurynlb"
   internal           = false
   load_balancer_type = "network"
-  
   subnets            = [aws_subnet.pub-frontend.id]
   
-  
-
   tags = {
     Name = "MercuryNLB80"
   }
 }
-  
+
+#nlb add target group still not working
+resource "aws_lb_listener" "nlb_front_end" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port              = "443"
+  protocol          = "TCP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.httpsTG.arn
+  }
+}
 
 
 
 #create target group 
-resource "aws_lb_target_group" "httpTG" {
-  name     = "FrontendHttpTG"
-  port     = 80
-  protocol = "HTTP"
+resource "aws_lb_target_group" "httpsTG" {
+  name     = "FrontendHttpsTG"
+  port              = "80"
+  protocol = "TCP"
   target_type = "instance"
   vpc_id   = aws_vpc.mainvpc.id
 
   health_check {
+      
+      protocol = "HTTPS"
       healthy_threshold = 2
-      unhealthy_threshold = 3
-      timeout = 60
-      interval = 100
+      interval = 30
   }
   
   tags = {
-    Name = "FrontendHttpTG"
+    Name = "FrontendHttpsTG"
   }
   
 } 
@@ -41,16 +48,18 @@ resource "aws_lb_target_group" "httpTG" {
 
 #add frontend and backend to targetgroup
 resource "aws_lb_target_group_attachment" "lbbackend" {
-  target_group_arn = aws_lb_target_group.httpTG.id
+  target_group_arn = aws_lb_target_group.httpsTG.id
   target_id        = aws_instance.ec2Backend.id
   port             = 80
+  
 
   
 }
  
 
 resource "aws_lb_target_group_attachment" "lbfrontend" {
-  target_group_arn = aws_lb_target_group.httpTG.id
+  target_group_arn = aws_lb_target_group.httpsTG.id
   target_id        = aws_instance.ec2webserver.id
   port             = 80
+  
 } 
